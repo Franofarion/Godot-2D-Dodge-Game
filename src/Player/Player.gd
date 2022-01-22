66d72d99a5a72588a123979059e07e1a85de2295
Baptiste Domange <baptiste.domange@gmail.com>
2022-01-22 17:00:32 +0100

    first commit

---
+++ src/Player/Player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+# if you hold right and down at the same time, the resulting velocity 
+# vector will be (1, 1). In this case, since we're adding a horizontal and 
+# a vertical movement, the player would move faster diagonally than if 
+# it just moved horizontally.
+
+# We can prevent that if we normalize the velocity, which means we set its length
+# to 1, then multiply by the desired speed. This means no more fast diagonal movement.
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
